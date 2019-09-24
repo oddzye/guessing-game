fc07b145f054eaac3743800186331d987ea6d744
oddzye <tzariov.dmitry@yandex.ru>
2019-09-24 17:10:04 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = null;
+        this.min = null;
+        this.guessNumber = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        
     }
 
     guess() {
-
+        this.guessNumber = Math.ceil((this.max + this.min) / 2);
+        return this.guessNumber;
     }
 
     lower() {
-
+        this.max = this.guessNumber;
     }
 
     greater() {
-
+        this.min = this.guessNumber;
     }
 }
 
